3ae4bb321fbbcefb18b45286b5e447e43985463d
gerard lucas <93461495+gerard0lucas@users.noreply.github.com>
2023-05-24 13:30:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/todo", require("./routes/todorouts"));
+app.use("/api/blogs", require("./routes/todorouts"));
 
 // app.use(errorhandler
 
